d02986f51bd35b0d08051aa1eef5f72b5ed6860d
Wei-bin-Wu <weibin789123@gmail.com>
2021-10-18 16:13:13 +0200

    Episode 24

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,5 +16,8 @@ class Post extends Model
         return 'slug';
     }
 
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
 
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,6 +7,9 @@
                 </a>
             </h1>
 
+            <p>
+                <a href="#">{{ $post->category->namegi }}</a>
+            </p>
             <div>
                 {{ $post->excerpt }}
             </div>
@@ -14,73 +17,3 @@
     @endforeach
 </x-layout>
 
-
-
-
-
-{{--<x-layout >--}}
-{{--    <x-slot name="content">--}}
-{{--        hello--}}
-{{--    </x-slot>--}}
-{{--</x-layout>--}}
-
-
-
-
-{{--@extends('layout')--}}
-
-{{--@section('header')--}}
-{{--    <h1>header</h1>--}}
-{{--@endsection--}}
-
-{{--@section('content')--}}
-{{--    @foreach($posts as $post)--}}
-{{--        <article>--}}
-{{--            <h1>--}}
-{{--                <a href="/posts/{{$post->slug}}">--}}
-{{--                    {{ $post->title  }}--}}
-{{--                </a>--}}
-{{--            </h1>--}}
-
-{{--            <div>--}}
-{{--                {{ $post->expert }}--}}
-{{--            </div>--}}
-{{--        </article>--}}
-{{--    @endforeach--}}
-{{--@endsection--}}
-
-{{--<!DOCTYPE html>--}}
-{{--    <title>My Blog</title>--}}
-{{--    <link rel="stylesheet" href="/app.css">--}}
-
-{{--    <body>--}}
-{{--        <article>--}}
-{{--            <h1><a href="/posts/my-first-post">My first post</a></h1>--}}
-{{--            <p>jalsdkjwofihsjhfoijoiawjfkjsjfioaiwjfi</p>--}}
-{{--        </article>--}}
-
-{{--        <article>--}}
-{{--            <h1><a href="/post/my-second-post">My second post</a></h1>--}}
-{{--            <p>jalsdkjwofihsjhfoijoiawjfkjsjfioaiwjfi</p>--}}
-{{--        </article>--}}
-
-{{--        <article>--}}
-{{--            <h1><a href="/post/my-third-post">My third post</a></h1>--}}
-{{--            <p>jalsdkjwofihsjhfoijoiawjfkjsjfioaiwjfi</p>--}}
-{{--        </article>--}}
-
-
-{{--        @foreach($posts as $post)--}}
-{{--            <article>--}}
-{{--                <h1>--}}
-{{--                    <a href="/posts/{{$post->slug}}">--}}
-{{--                        {{ $post->title  }}--}}
-{{--                    </a>--}}
-{{--                </h1>--}}
-
-{{--                <div>--}}
-{{--                    {{ $post->expert }}--}}
-{{--                </div>--}}
-{{--            </article>--}}
-{{--        @endforeach--}}
-{{--    </body>--}}
