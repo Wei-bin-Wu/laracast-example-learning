27a62839df5db9faf79562b20529ff67c16fb203
Wei-bin-Wu <weibin789123@gmail.com>
2021-10-18 11:59:54 +0200

    Episode 20

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,10 @@
+{{--<!DOCTYPE html>--}}
+{{--<title>My Blog</title>--}}
+{{--<link rel="stylesheet" href="/app.css">--}}
+
+{{--<body>--}}
+{{--        <header>--}}
+{{--            @yield('header')--}}
+{{--        </header>--}}
+{{--    @yield('content')--}}
+{{--</body>--}}
