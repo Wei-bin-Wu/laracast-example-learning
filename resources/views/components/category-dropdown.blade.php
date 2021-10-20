0524f97668bc90eeec7ec023085032dee867d5d4
Wei-bin-Wu <weibin789123@gmail.com>
2021-10-20 09:19:43 +0200

    Episode 44:Pagination

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -5,11 +5,12 @@
             <x-icon name="down-arrow" class="absolute pointer-events-none" style="right: 12px;" />
         </button>
     </x-slot>
-    <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+    <x-dropdown-item href="/?{{ http_build_query(request()->except('category','page')) }}"
+                     :active="request()->routeIs('home')">All</x-dropdown-item>
 
     @foreach($categories as $category)
         <x-dropdown-item
-            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
+            href="/?category={{ $category->slug }}&{{ http_build_query(request()->except('category','page')) }}"
             {{-- http... convierte un array a query ['a'=>'b']  a  a=b& --}}
             {{--                        :active="isset($currentCategory) && $currentCategory->is($category)"--}}
             :active="request()->is('categories/'. $category->slug)" {{-- or '*'.$category->slug or 'categories/.{$category->slug}' --}}
